6709596c58647e49e61db3ca43d3e1942c89f921
Charith Jayarangana <cj.ptsz@yahoo.com>
2022-07-19 10:51:29 +0530

    increment i by 1 and then perform multiplication with n bcz ++i not supported in scala

--- 2/1.scala
+++ 2/1.scala
@@ -1,8 +1,8 @@
 object Main extends App {
     var i,j,k:Int = 2;
     var m,n:Int = 5;
-    var f:Double=12.0f;
-    var g:Double=4.0f;
+    var f:Float=12.0f;
+    var g:Float=4.0f;
     var c:Char='X';
     // printf("i = %c,j = %f,k = %f",c,f,g);     // scala doesn't support ++i
 
@@ -11,6 +11,7 @@ object Main extends App {
     printf("%d\n",n%j);
     printf("%d\n",m/j*j);
     printf("%f\n",f + 10*5 + g);
-    // print(++i*n);
+    (i+=1);
+    printf("%d\n",i*n);// print(++i*n);
 
 }
