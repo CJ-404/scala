63bdaa96f13570267d76b585988266a68ac7885f
Charith Jayarangana <cj.ptsz@yahoo.com>
2022-08-01 16:13:21 +0530

    finished all errors and deleted some because tutorial changed

--- 4/2.scala
+++ 4/2.scala
@@ -1,37 +1,17 @@
 object Main extends App{
 
-    case class shopping_cart(quantity:Int , name:String, price:Double);
-
-    def print_cart(a:Array[shopping_cart]):Any = {
-        if(a.length >= 1){
-            println(a.head.quantity + " " + a.head.name + " at Rs " +a.head.price+ " each");
-            // println("$a.head.quantity $a.head.name at Rs $a.head.price each");
-            print_cart(a.tail);
-        }
-        
+    def check(n:Int):Any= n match{
+        case 0 => println("Zero is given");
+        case x if(x < 0) => println("Negative number is given");
+        case x if(x%2 == 0 ) => println("Even number is given");
+        case _ => println("Odd number is given");
     }
 
-    def out_vanilla(a:Array[shopping_cart]):Any = {
-        if(a.length >=1){
-            if(a.head.name == "vanilla ice cream"){
-                println("Found " + a.head.quantity + " " +a.head.name);
-            }
-            else{
-                println("Found another item");
-            }
-            out_vanilla(a.tail);
-        }
+    def getInput():Any = {
+        printf("Enter a number : ");
+        check(scala.io.StdIn.readInt());
     }
 
-    val item1  = shopping_cart(13,"vanilla ice cream",3.99);
-    val item2 = shopping_cart(6,"chocolate biscuits",4.00);
-    val item3 = shopping_cart(7,"cupcakes",7.77);
-
-
-    val a = Array(item1,item2,item3);
-    print_cart(a);
-    println("\n\nMethod for output vanilla ice cream\n");
-    out_vanilla(a);
-
-
+    getInput();
+    
 }
--- 4/3.scala
+++ 4/3.scala
@@ -1,42 +1,21 @@
 object Main extends App{
 
-    def hashcode(a:String,b:Int = 0):Long = {
-        if(a.length >= 1){    
-            ((a.head.toLong)*(scala.math.pow(10,b).toLong)) + hashcode(a.tail,b+1);
-        }
-        else{
-            0;
-        }
+    def toUpper(str:String):String = str.toUpperCase();
+
+    def toLower(str:String):String = str.toLowerCase();
+
+    def first_2_Upper(str:String):String = (str.substring(0,2)).toUpperCase() + str.substring(2,str.length);
+
+    def last_Upper(str:String):String = str.substring(0,str.length-1) + (str(str.length-1)).toUpper;
+
+    def formatNames(name:String,func:(String)=>String):String = {
+        func(name);
     }
 
-    case class car(name:String,price:Double);
-
-    def genrate_UUID(a:Array[car],b:Array[String],index:Int = 0):Any = {
-        if(a.length >= 1){
-            b(index) = a.head.name +" - " + hashcode(a.head.name).toString;
-            genrate_UUID(a.tail,b,index+1);
-        }
-    }
-
-    def print_uuid(a:Array[car],b:Array[String]):Any = {
-        if(a.length>=1){
-            println("car uuid = " + b.head);
-            print_uuid(a.tail,b.tail);
-        }
-    }
-
-    val car1 = car("bmw 3 series",20000);
-    val car2 = car("bmw 5 series",50000);
-    val car3 = car("vw passat",10000);
-    val car4 = car("vw golf",12000);
-    val car5 = car("mazda 3",15000);
-
-    val car_inventory = Array(car1,car2,car3,car4,car5);
-    val hashcodes = new Array[String](car_inventory.length);
-
-    
-    genrate_UUID(car_inventory,hashcodes);
-    print_uuid(car_inventory,hashcodes);
+    println(formatNames("Benny",toUpper));
+    println(formatNames("Niroshan",first_2_Upper));
+    println(formatNames("Saman",toLower));
+    println(formatNames("Kumara",last_Upper));
 
     
 }
